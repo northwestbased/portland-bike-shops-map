54b8f4c1eee2bf8160c7adcba5b350dea507d9a0
northwestbased <northwestbased@gmail.com>
2016-10-21 15:52:42 -0700

    reworked everything

--- script.js
+++ script.js
@@ -1,89 +1,122 @@
-$("input[type='submit']").click(function() {
-    var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + $("input[type='text']").val() + " portland";
-    $.get(url, function(data) {
-        my_location = data.results[0].geometry.location;
-        var marker = new google.maps.Marker({
-            position: my_location,
-            map: map,
-            title: "Your Location",
-            icon: "blue_icon.png"
-        });
-        map.panTo(my_location);
-        closest_bike_shop = bike_shops[0];
-        closest_distance = undefined;
+$("button").click(function() {
+  var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" +
+    $("input[type='text']").val() + " portland";
 
-        for (var i = 1; i < bike_shops.length; ++i) {
-            var bike_shop = bike_shops[i];
-            delta_lat = my_location.lat - bike_shop.location.lat
-            delta_long = Math.abs(my_location.lng) - Math.abs(bike_shop.location.lng);
-            distance = Math.sqrt(delta_lat * delta_lat + delta_long * delta_long)
-            if (closest_distance == undefined || distance < closest_distance) {
-                closest_distance = distance;
-                closest_bike_shop = bike_shop;
-            }
-        }
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        directionsDisplay.setMap(map);
-        directionsDisplay.setOptions({
-            suppressMarkers: true
-        });
-        directionsService.route({
-            origin: my_location,
-            destination: closest_bike_shop.location,
-            travelMode: 'BICYCLING'
-        }, function(response, status) {
-            directionsDisplay.setDirections(response);
-        });
+  $.get(url, function(data) {
+    my_location = data.results[0].geometry.location;
+    var marker = new google.maps.Marker({
+      position: my_location,
+      map: map,
+      icon: "purple_icon.png"
     });
+    map.panTo(my_location);
 
-    $("#map").animate({
-        opacity: 1
-    }, 1000);
-    $("#map").css("pointer-events", "all");
-    $("#start").fadeOut(800);
+    var info_window = new google.maps.InfoWindow();
+    info_window.setContent("Your Location");
+    info_window.open(map, marker);
+
+    for (var i = 1; i < bike_shops.length; ++i) {
+      delta_lat = my_location.lat - bike_shops[i].location.lat
+      delta_long = Math.abs(my_location.lng) - Math.abs(bike_shops[i].location.lng);
+      distance = Math.sqrt(delta_lat * delta_lat + delta_long * delta_long)
+      bike_shops[i].distance = distance;
+    }
+    bike_shops.sort(function(a, b) {
+      var x = a.distance;
+      var y = b.distance;
+      if (x < y)
+        return -1;
+      return 1;
+    });
+    renderShopDetails(bike_shops.slice(0,5));
+  });
 });
 
+
+function show_directions(start, end){
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
+    directionsDisplay.setOptions({
+      suppressMarkers: true
+    });
+    directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: 'BICYCLING'
+    }, function(response, status) {
+      directionsDisplay.setDirections(response);
+    });
+}
+
 $.getJSON("bike.json", function(json) {
-    bike_shops = json["bike-shops"];
-    initMap_wait();;
+  bike_shops = json["bike-shops"];
+  initMap_wait();;
 });
 
 function initMap_wait() {
-    if (typeof google == "undefined")
-        setTimeout(initMap_wait, 500)
-    else
-        initMap();
+  if (typeof google == "undefined")
+    setTimeout(initMap_wait, 500)
+  else
+    initMap();
 }
 
 var map;
 
 function initMap() {
-    var info_window = new google.maps.InfoWindow();
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 45.5212,
+      lng: -122.6271
+    },
+    zoom: 13
+  });
+  initMarkers();
+}
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 45.52,
-            lng: -122.681944
-        },
-        zoom: 15
+var previous_icon;
+
+function initMarkers() {
+  var markers = [];
+  for (var i = 0; i < bike_shops.length; ++i) {
+    var data = bike_shops[i];
+    var marker = new google.maps.Marker({
+      position: data["location"],
+      map: map,
+      title: data["name"],
+      icon: "green_icon.png"
     });
-
-    var markers = [];
-    for (var i = 0; i < bike_shops.length; ++i) {
-        var data = bike_shops[i];
-        var marker = new google.maps.Marker({
-            position: data["location"],
-            map: map,
-            title: data["name"]
+    (function(marker, data) {
+      google.maps.event.addListener(marker, 'click',
+        function(event) {
+          renderShopDetails(data);
+          if (previous_icon != undefined)
+            previous_icon.setIcon("green_icon.png");
+          marker.setIcon("blue_icon.png");
+          previous_icon = marker;
         });
-        (function(marker, data) {
-            google.maps.event.addListener(marker, 'click', function(event) {
-                var html_content = "<div class='title'><a target='_blank' href='" + data["website"] + "'>" + data["name"] + "</a></div>" +
-                    "<div class='address'>" + data["address"] + "</div><div class='number'>" + data["number"] + "</div>";
-                info_window.setContent(html_content);
-                info_window.open(map, marker);
-            });
-        })(marker, data);
-    }
+    })(marker, data);
+  }
+}
+
+
+function renderShopDetails(shops) {
+  if (!Array.isArray(shops)) {
+    shop = shops;
+    shops = [];
+    shops[0] = shop;
+  }
+  $("#shop_detail").html('');
+  for (shop in shops) {
+    shop = shops[shop];
+    var html_content = 
+      "<div class='title'>" + shop["name"] + "</div>"+
+      "<a target=" +
+      "'_blank' href='" + shop["website"] + "'>" +
+      shop["website"] + "</a>" +
+      "<div class='address'>" + shop["address"] +
+      "</div><div class='number'>" + shop["number"] +
+      "</div><div class='hr skinny'></div>";
+    $("#shop_detail").append(html_content);
+  }
 }
